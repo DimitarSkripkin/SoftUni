fa0e9b07540c6466ff102a7e42816ed7b4b38a5d
Dimitar Skripkin <m1taka2217@yahoo.com>
2015-05-13 20:53:14 +0300

    force conflict

--- Teamwork/01. Source Control Systems/lab 2 - algorithms/02. The Sieve of Eratosthenes/Program.cs
+++ Teamwork/01. Source Control Systems/lab 2 - algorithms/02. The Sieve of Eratosthenes/Program.cs
@@ -17,19 +17,6 @@ namespace _02.The_Sieve_of_Eratosthenes {
 		}
 		static void Main( string[] args ) {
 			int n = int.Parse( Console.ReadLine() );
-			//List<int> arr = Enumerable.Range( 2, n - 1 ).ToList();
-
-			//int p = 2;
-			//while ( p < n ) {
-			//	for ( int i = 0; i < n - 1; ++i ) {
-			//		if ( arr[ i ] % p == 0 && arr[i] != p ) {
-			//			arr[ i ] = -1;
-			//		}
-			//	}
-			//	++p;
-			//}
-			//arr.RemoveAll( item => item == -1 );
-			//Console.WriteLine(string.Join(", ",arr));
 			var list = new List<int>();
 			for ( int i = 2; i < n; ++i ) {
 				if ( IsPrime( i ) ) {
